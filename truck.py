e35241d8a5d9a620f2887c76d2b26ee7cab56a16
chris alvizuris <chris.alvizuris@gmail.com>
2021-04-08 00:39:05 -0700

    Able to send out trucks and update package statuses.

--- truck.py
+++ truck.py
@@ -1,5 +1,5 @@
 import collections
-from datetime import time, timedelta
+import datetime
 
 # create a truck class that is initialized with data about the truck
 from graph import Vertex
@@ -12,10 +12,11 @@ class Truck:
         self.graph = graph
         self.start_location = '4001 South 700 East'
         self.current_location = '4001 South 700 East'
+        self.previous_location = None
         self.mph = 18.0
         self.max_packages = 16
-        self.time = time()
-        self.time_left_hub = time()
+        self.time = datetime.datetime(1, 1, 1, 1, 1, 1)
+        self.time_left_hub = datetime.datetime(1, 1, 1, 1, 1, 1)
         self.package_list = collections.deque()
         self.path = collections.deque()
         self.distance_traveled = 0.0
@@ -160,7 +161,10 @@ def calculate_path_miles(truck):
     return truck.path_miles
 
 
-print(calculate_path_miles(truck_1))
+print('Truck 1 path miles:', calculate_path_miles(truck_1))
+print('Truck 2 path miles:', calculate_path_miles(truck_2))
+print('Truck 3 path miles:', calculate_path_miles(truck_3))
+
 
 # return the number of miles between two addresses
 def miles_between_locations(location1, location2):
@@ -168,6 +172,72 @@ def miles_between_locations(location1, location2):
 
 
 
-# def send_out_trucks(truck_1, truck_2, truck_3):
-#     truck1.time_left_hub = time(hour=08)
-#     print()
+def send_out_trucks(truck_1, truck_2, truck_3):
+    truck_1.time_left_hub = datetime.datetime(100, 1, 1, 8, 0, 0)
+    truck_1.time = datetime.datetime(100, 1, 1, 8, 0, 0)
+    truck_2.time_left_hub = datetime.datetime(100, 1, 1, 9, 5, 1)
+    truck_2.time = datetime.datetime(100, 1, 1, 9, 5, 1)
+    truck_3.time_left_hub = datetime.datetime(100, 1, 1, 9, 5, 1)
+    truck_3.time = datetime.datetime(100, 1, 1, 9, 5, 1)
+
+    # send out truck 1
+    truck_1.path.popleft()
+    truck1_path_copy = list(truck_1.path)
+    for i, path in enumerate(truck1_path_copy):
+        truck_1.distance_traveled += distance_dict[truck_1.current_location, truck1_path_copy[i]]
+        distance_in_minutes = 3.33 * distance_dict[truck_1.current_location, truck1_path_copy[i]]
+        # need to add the time ... possibly need to use datetime instead of time objects
+
+        minutes_added = datetime.timedelta(minutes=distance_in_minutes)
+        truck_1.time = truck_1.time + minutes_added
+        for package in range(len(truck_1.package_list)):
+            if truck_1.package_list[package].address == truck1_path_copy[i]:
+                print('package address is equal to path')
+                truck_1.package_list[package].status = "delivered"
+                # truck_1.package_list.remove(truck_1.package_list[package])
+        truck_1.current_location = truck1_path_copy[i]
+        print('Truck 1 path:', truck_1.path)
+        truck_1.path.popleft()
+        print('Truck 1 made a stop at', truck_1.time.time())
+    print('Truck 1 package list:', truck_1.package_list[1])
+
+    print('')
+    # send out truck 2
+    truck_2.path.popleft()
+    truck2_path_copy = list(truck_2.path)
+    for i, path in enumerate(truck2_path_copy):
+        truck_2.distance_traveled += distance_dict[truck_2.current_location, truck2_path_copy[i]]
+        distance_in_minutes2 = 3.33 * distance_dict[truck_2.current_location, truck2_path_copy[i]]
+
+        minutes_added = datetime.timedelta(minutes=distance_in_minutes2)
+        truck_2.time = truck_2.time + minutes_added
+        for package in range(len(truck_2.package_list)):
+            if truck_2.package_list[package].address == truck2_path_copy[i]:
+                truck_2.package_list[package].status = "delivered"
+                # truck_2.package_list.remove(package)
+        truck_2.current_location = truck2_path_copy[i]
+        print('Truck 2 path:', truck_2.path)
+        truck_2.path.popleft()
+        print('Truck 2 made a stop at', truck_2.time.time())
+
+    print('')
+    # send out truck 3
+    truck_3.path.popleft()
+    truck3_path_copy = list(truck_3.path)
+    for i, path in enumerate(truck3_path_copy):
+        truck_3.distance_traveled += distance_dict[truck_3.current_location, truck3_path_copy[i]]
+        distance_in_minutes3 = 3.33 * distance_dict[truck_3.current_location, truck3_path_copy[i]]
+
+        minutes_added = datetime.timedelta(minutes=distance_in_minutes3)
+        truck_3.time = truck_3.time + minutes_added
+        for package in range(len(truck_3.package_list)):
+            if truck_3.package_list[package].address == truck3_path_copy[i]:
+                truck_3.package_list[package].status = "delivered"
+                # truck_3.package_list.remove(package)
+        truck_3.current_location = truck3_path_copy[i]
+        print('Truck 3 path:', truck_3.path)
+        truck_3.path.popleft()
+        print('Truck 3 made a stop at', truck_3.time.time())
+
+
+send_out_trucks(truck_1, truck_2, truck_3)
